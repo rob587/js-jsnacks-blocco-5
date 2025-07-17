7c19686488f0d1cded6b74e07db4febc6b7382fc
rob587 <robertocamm02@yahoo.com>
2025-07-17 16:46:47 +0200

    quinto snack completato

--- js-jsnacks-blocco-5/snack-5/script.js
+++ js-jsnacks-blocco-5/snack-5/script.js
@@ -2,3 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const getEvenNumbers = (inputArray) => {
+  return nums.filter(number => number % 2 === 0);
+};
+
+
+const evenNumbers = getEvenNumbers(nums);
+console.log(getEvenNumbers());
