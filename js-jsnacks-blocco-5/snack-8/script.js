21fa29bae98a66e07508009b37525d7dd85bdd08
rob587 <robertocamm02@yahoo.com>
2025-07-17 22:47:09 +0200

    ottavo  snack terminato

--- js-jsnacks-blocco-5/snack-8/script.js
+++ js-jsnacks-blocco-5/snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+const studentsAfterFilter = students.filter((student) => {
+
+    return student.name === "Marco Lanci" ;
+}) [0]
+
+console.log(studentsAfterFilter.class)
