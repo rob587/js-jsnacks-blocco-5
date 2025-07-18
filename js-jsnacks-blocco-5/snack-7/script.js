d5a6778d454b09a60d9fbfd8e10fc24b7f5cf8b3
rob587 <robertocamm02@yahoo.com>
2025-07-18 09:59:31 +0200

    [fix] settimo snack

--- js-jsnacks-blocco-5/snack-7/script.js
+++ js-jsnacks-blocco-5/snack-7/script.js
@@ -22,9 +22,9 @@ const students = [
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
 
-const studentsAfterFilter = students.filter((student) => {
+const studentsAfterFilter = students.find((student) => {
 
     return student.id === 2;
-}) [0]
+}) 
 
 console.log(studentsAfterFilter)
